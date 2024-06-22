a093bf42e738afcbe6e17fa02b192925d2c0b0c6
R. Vijay Ram <vijayrram.ae@gmail.com>
2024-06-22 10:36:14 +0530

    feat(fizzbuzz-v2): Add support for version 2

---
+++ fizzbuzz/Python/fizzbuzz2.py
@@ -0,0 +1,26 @@
+"""FizzBuzz Coding Kata"""
+
+from fizzbuzz_rules import FIZZBUZZ_RULES, apply_rule
+
+
+def fizzbuzz2(number: int) -> str | int:
+    """Helper used to modify input integers to the correct response for the FizzBuzz game version 2.
+
+    Args:
+        number (int): Number to be modified.
+
+    Returns:
+        str: Response to be uttered instead.
+    """
+
+    if not isinstance(number, int):
+        raise TypeError("Invalid input type.")
+
+    output = "".join(apply_rule(number, rule) for rule in FIZZBUZZ_RULES)
+
+    return output if output else str(number)
+
+
+if __name__ == "__main__":
+    for num in range(1, 101):
+        print(fizzbuzz2(num))
---
+++ fizzbuzz/Python/test_fizzbuzz2.py
@@ -0,0 +1,70 @@
+"""Test script for the FizzBuzz Kata version 2."""
+
+import typing
+import pytest
+
+from fizzbuzz2 import fizzbuzz2
+
+@pytest.mark.parametrize("number, result", [(1, "1"), (2, "2")])
+def test_fizzbuzz2(number: int, result):
+    """Test script to check the response of the fizzbuzz2 function.
+
+    Args:
+        number (int): Input number for the fizzbuzz2 function.
+        result (str): The expected response from the fizzbuzz2 function.
+    """
+
+    response = fizzbuzz2(number)
+
+    assert response == result
+
+
+@pytest.mark.parametrize("number", ["1", 1.0])
+def test_fizzbuzz2_invalid_input_type(number: typing.Any):
+    """Test script to check the response of the fizzbuzz2 function for invalid input types.
+
+    Args:
+        number (Any): Input for the fizzbuzz2 function.
+    """
+
+    with pytest.raises(TypeError, match=r"^Invalid input type.$"):
+        fizzbuzz2(number)
+
+
+@pytest.mark.parametrize("number", [6, 9])
+def test_fizzbuzz2_div_by_3_only_but_not_containing_3_or_5(number: int):
+    """Test script to check the response of the fizzbuzz2 function for numbers divisible by 3 only.
+
+    Args:
+        number (int): Input number for the fizzbuzz2 function.
+    """
+
+    response = fizzbuzz2(number)
+
+    assert response == "Fizz"
+
+
+@pytest.mark.parametrize("number", [10, 20])
+def test_fizzbuzz2_div_by_5_only_but_not_containing_3_or_5(number: int):
+    """Test script to check the response of the fizzbuzz2 function for numbers divisible by 5 only.
+
+    Args:
+        number (int): Input number for the fizzbuzz2 function.
+    """
+
+    response = fizzbuzz2(number)
+
+    assert response == "Buzz"
+
+
+@pytest.mark.parametrize("number", [60, 90])
+def test_fizzbuzz2_div_by_15_but_not_containing_3_or_5(number: int):
+    """Test script to check the response of the fizzbuzz2 function for numbers divisible by 15.
+
+    Args:
+        number (int): Input number for the fizzbuzz2 function.
+    """
+
+    response = fizzbuzz2(number)
+
+    assert response == "FizzBuzz"
