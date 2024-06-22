a3e8e8066a7670fd3b79766d7376282fee9d85cc
R. Vijay Ram <vijayrram.ae@gmail.com>
2024-06-23 01:51:02 +0530

    refactor(fizzbuzz-v2): Use decorators to generate functions

--- fizzbuzz/Python/fizzbuzz2.py
+++ fizzbuzz/Python/fizzbuzz2.py
@@ -1,26 +1,42 @@
 """FizzBuzz Coding Kata"""
 
+from typing import Callable, TypeAlias
 from fizzbuzz_rules import apply_contains_rule, apply_divisibility_rule
 
+FizzBuzzRules: TypeAlias = list[Callable[[int], str]]
 
-def fizzbuzz2(number: int) -> str | int:
-    """Helper used to modify input integers to the correct response for the FizzBuzz game version 2.
+
+def fizzbuzz_response_generator(fizzbuzz_rules: FizzBuzzRules) -> Callable[[int], str]:
+    """Factory method for creating FizzBuzz responders.
 
     Args:
-        number (int): Number to be modified.
+        fizzbuzz_rules (FizzBuzzRules): List of rules to be applied.
 
     Returns:
-        str: Response to be uttered instead.
+        Callable[[int], str]: The generated responder.
     """
 
-    if not isinstance(number, int):
-        raise TypeError("Invalid input type.")
+    def inner(number: int) -> str:
+        """Responder to be returned.
 
-    output = ""
-    output += apply_contains_rule(number)
-    output += apply_divisibility_rule(number)
+        Args:
+            number (int): Number on which the rules should be applied.
 
-    return output if output else str(number)
+        Returns:
+            str: Reponse obtained after applying the rules.
+        """
+
+        if not isinstance(number, int):
+            raise TypeError("Invalid input type.")
+
+        output = "".join(rule(number) for rule in fizzbuzz_rules)
+
+        return output if output else str(number)
+
+    return inner
+
+
+fizzbuzz2 = fizzbuzz_response_generator([apply_contains_rule, apply_divisibility_rule])
 
 
 if __name__ == "__main__":
