619d1c95eaccec0bca2dcf6dded5f760632b093e
R. Vijay Ram <vijayrram.ae@gmail.com>
2024-06-23 01:56:28 +0530

    refactor(fizzbuzz-v2): Reduce file interdependancy

--- fizzbuzz/Python/fizzbuzz2.py
+++ fizzbuzz/Python/fizzbuzz2.py
@@ -1,42 +1,6 @@
 """FizzBuzz Coding Kata"""
 
-from typing import Callable, TypeAlias
-from fizzbuzz_rules import apply_contains_rule, apply_divisibility_rule
-
-FizzBuzzRules: TypeAlias = list[Callable[[int], str]]
-
-
-def fizzbuzz_response_generator(fizzbuzz_rules: FizzBuzzRules) -> Callable[[int], str]:
-    """Factory method for creating FizzBuzz responders.
-
-    Args:
-        fizzbuzz_rules (FizzBuzzRules): List of rules to be applied.
-
-    Returns:
-        Callable[[int], str]: The generated responder.
-    """
-
-    def inner(number: int) -> str:
-        """Responder to be returned.
-
-        Args:
-            number (int): Number on which the rules should be applied.
-
-        Returns:
-            str: Reponse obtained after applying the rules.
-        """
-
-        if not isinstance(number, int):
-            raise TypeError("Invalid input type.")
-
-        output = "".join(rule(number) for rule in fizzbuzz_rules)
-
-        return output if output else str(number)
-
-    return inner
-
-
-fizzbuzz2 = fizzbuzz_response_generator([apply_contains_rule, apply_divisibility_rule])
+from fizzbuzz_rules import fizzbuzz2
 
 
 if __name__ == "__main__":
--- fizzbuzz/Python/fizzbuzz_rules.py
+++ fizzbuzz/Python/fizzbuzz_rules.py
@@ -1,6 +1,9 @@
 """Rules for FizzBuzz."""
 
-from typing import NamedTuple
+from typing import Callable, NamedTuple, TypeAlias
+
+
+FizzBuzzRules: TypeAlias = list[Callable[[int], str]]
 
 
 class FizzBuzzRule(NamedTuple):
@@ -46,3 +49,37 @@ def apply_contains_rule(number: int) -> str:
         if str(rule.divisor) in str(number)
         else "" for rule in FIZZBUZZ_RULES
     )
+
+
+
+def fizzbuzz_response_generator(fizzbuzz_rules: FizzBuzzRules) -> Callable[[int], str]:
+    """Factory method for creating FizzBuzz responders.
+
+    Args:
+        fizzbuzz_rules (FizzBuzzRules): List of rules to be applied.
+
+    Returns:
+        Callable[[int], str]: The generated responder.
+    """
+
+    def inner(number: int) -> str:
+        """Responder to be returned.
+
+        Args:
+            number (int): Number on which the rules should be applied.
+
+        Returns:
+            str: Reponse obtained after applying the rules.
+        """
+
+        if not isinstance(number, int):
+            raise TypeError("Invalid input type.")
+
+        output = "".join(rule(number) for rule in fizzbuzz_rules)
+
+        return output if output else str(number)
+
+    return inner
+
+
+fizzbuzz2 = fizzbuzz_response_generator([apply_contains_rule, apply_divisibility_rule])
